f361e4af44f3366a07e16dc727fab83a52ed36db
Willjfield <willjfield@gmail.com>
2017-05-18 16:46:30 -0400

    day one

--- 2017-portfolio/public/js/app.js
+++ 2017-portfolio/public/js/app.js
@@ -5724,27 +5724,56 @@ $(document).ready(function(){
 
 	//Handle the project thumbs
 	$('.project img').mouseenter(function(event) {
-		TweenLite.to($(this).parent().find('h1.project-title'), .3, {opacity:0, onComplete:function(){
-			console.log('done trans')
-			$(this).parent().find('h1.project-title').css('z-index',-1);
-		}});
+		if(!$(this).hasClass('project-active')){
+			$(this).addClass('colorize');
+			// TweenLite.to($(this).parent().find('h1.project-title'), .3, {opacity:0, onComplete:function(){
+			// 	console.log('done trans')
+			// 	$(this).parent().find('h1.project-title').css('z-index',-1);
+			// }});
+		}
 	});
 
 	$('.project img').mouseleave(function(event) {
-		TweenLite.to($(this).parent().find('h1.project-title'), .3, {opacity:1, onComplete:function(){
-			$(this).parent().find('h1.project-title').css('z-index',1);
-		}});
+		if(!$(this).hasClass('project-active')){
+			$(this).removeClass('colorize');
+			// TweenLite.to($(this).parent().find('h1.project-title'), .3, {opacity:1, onComplete:function(){
+			// 	$(this).parent().find('h1.project-title').css('z-index',1);
+			// }});
+		}
+	});
+
+	$('.project').on('click',function(){
+		if(!$(this).find('img').hasClass('project-active')){
+			$('.project').find('img').addClass('project-active');
+			//$().find('img').removeClass('colorize');
+			$(this).find('.project-description-container').addClass('project-active');
+			$('body').css('overflow','hidden');
+		}else{
+			$('.project').find('img').removeClass('project-active');
+			$(this).find('img').removeClass('colorize');
+			$(this).find('.project-description-container').removeClass('project-active');
+			$('body').css('overflow','visible');
+		}
+		
 	});
 
 	sizeElements();
 });
 
+//EVENTS
 $(window).resize(function(){
 	sizeElements();
 });
+
+var lastScroll=0;
+$(window).scroll(function(e){
+	uniforms.u_time.value += ($(window).scrollTop()-lastScroll)*.02;
+	lastScroll = $(window).scrollTop();
+})
 
 function sizeElements(){
 	$('h1.project-title').each(function(index, el) {
 		$(el).css('top',$(el).innerHeight());
 	});
 }
+
